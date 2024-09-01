0e4b086873de32244f0fd5cdb1aa1e1c79ed4c83
gtklr <redzaramzan@gmail.com>
2024-09-01 21:40:48 +0900

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavbarSimple from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Muratone | DJ Producer",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         <NavbarSimple />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
